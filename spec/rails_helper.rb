3f5d486772c794f1e793e32f704e35df062f6991
tmhammer <timh@govwizely.com>
2017-11-13 11:42:37 -0500

    Revert to Rails 4.0.9

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -5,8 +5,6 @@ require File.expand_path('../dummy/config/environment', __FILE__)
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'spec_helper'
 require 'rspec/rails'
-require 'rails-controller-testing'
-Rails::Controller::Testing.install
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
